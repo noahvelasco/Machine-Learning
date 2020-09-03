10c068b4427dd97d4aff05e8ac5d9b1a79955cdd
Noah <npizza10@gmail.com>
2020-09-03 02:07:29 -0600

    Finished excercise code

--- Excercises/Excercise2/Padilla_Rafael_Performance_Evaluation.py
+++ Excercises/Excercise2/Padilla_Rafael_Performance_Evaluation.py
@@ -2,7 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time 
 
+"""
+To understand precision and recall go to the link below:
+    https://www.bmc.com/blogs/confusion-precision-recall/
+"""
 
+
+
+#(Total classification matches)/(Total Classifications)
 def accuracy(y_true,y_pred):
     return np.sum(y_true==y_pred)/y_true.shape[0]
 
@@ -29,16 +36,20 @@ def confusion_matrix(y_true,y_pred):
     return cm
 
 '''
-    precision() - Defined as (also called positive predictive value) is the 
-                    fraction of relevant instances among the retrieved instances [wikipedia] 
+    precision() - Defined as: (also called positive predictive value) is total amount
+                you predicted correct(True Positive) out of the total amount
+                you predicted correct plus total amount you thought you predicted correct (False Positive)
                         
                 Precision = (True Positive)/ (True Positive + False Positive)
+                false positive = a value predicted as positive but is actually negative
+                               = a value predicted as 'positive_class' but is actually not that class
                 
                 Goal - Calculate the precision of each classification (0-9)
                 
                 Algorithm: 
                     1. For each classification go through both arrays(10 times^)
-                        i. Keep count of total matched (True Positive) for each classification and total not matched (False positives)
+                        i. Keep count of True Positives for each classification 
+                            and total False positives
                     2. Plug true and false positives into the formula
 
 '''
@@ -50,46 +61,69 @@ def precision(y_true,y_pred,positive_class):
     #Could've used y_pred-doesnt matter since same size
     for i in range(len(y_true)):
         
-        #if true positive
-        if (y_true[i] == positive_class) and (y_true[i] == y_pred[i]):
+        #if true positive = if actual and pred match
+        if ( positive_class == y_true[i] ) and (y_true[i] == y_pred[i]):
             true_pos += 1
         
-        #if true negative
-        if (y_true[i] == positive_class) and (y_true[i] != y_pred[i]):
+        #if false positive = if predicted is positive_class but actually is not positive_class
+        if (y_pred[i] == positive_class) and (y_true[i] != y_pred[i]):
             false_pos +=1
    
     return true_pos / (true_pos + false_pos)
 
-#Modify
+
+
+'''
+    recall() - Defined as: (also known as sensitivity -wikipedia) is the total 
+                amount of matches predicted right(True Positive) out of 
+                total matches predicted right + total matches predicted wrong(false negatives). 
+                Its used to measure total amount of relevant items[wikipedia].
+                                
+                Recall = (True Positive) / (True Positive + False Negative)
+                
+                Goal - Calculate the recall of each classification(0-9)
+                
+                Algorithm(Similar to precision): 
+                    1. For each classification go through both arrays(10 times^)
+                        i. Keep count of total True Positives for each 
+                            classification and total False negatives
+                    2. Plug true and false positives into the formula
+'''
 def recall(y_true,y_pred,positive_class):
-    return 0.06572223
+    
+    true_pos = 0
+    false_neg = 0
+    
+    #Could've used y_pred-doesnt matter since same size
+    for i in range(len(y_true)):
+        
+        #if true positive = if actual and pred match
+        if ( positive_class == y_true[i] ) and (y_true[i] == y_pred[i]):
+            true_pos += 1
+        
+        #if false negative = if value should be positive_class but is predicted to not be positive class
+        if (y_true[i] == positive_class) and (y_true[i] != y_pred[i]):
+            false_neg +=1 
+    
+    rec = true_pos / (true_pos + false_neg)
+    
+    return rec
 
 
 
 if __name__ == '__main__':
    
     
-    #print('\nEvaluating Algorithm 1')
+    print('\nEvaluating Algorithm 1')
     y_test_a1 =  np.load('y_test_a1.npy')
     pred_a1 =  np.load('pred_a1.npy')
-    '''
-    #delete next two lines when done
-    print('Precision')
-    for i in range(np.amax(y_test_a1)+1):
-        precision(y_test_a1,pred_a1,i)
-    '''
-    
-    
     print('Accuracy:  {:.4}'.format(accuracy(y_test_a1,pred_a1)))
     print('Confusion matrix:')
     print(confusion_matrix(y_test_a1,pred_a1))
-    
-    
     print('Precision and recall:')
     for i in range(np.amax(y_test_a1)+1):
         print('Positive class {}, precision: {:.4}, recall: {:.4}'.format(i, precision(y_test_a1,pred_a1,i),recall(y_test_a1,pred_a1,i)))
     
-    '''
     print('\nEvaluating Algorithm 2')
     y_test_a2 =  np.load('y_test_a2.npy')
     pred_a2 =  np.load('pred_a2.npy')
@@ -97,6 +131,5 @@ if __name__ == '__main__':
     print('Precision and recall:')
     for i in range(np.amax(y_test_a2)+1):
         print('Positive class {}, precision: {:.4}, recall: {:.4}'.format(i, precision(y_test_a2,pred_a2,i),recall(y_test_a2,pred_a2,i)))
-    '''
     
     
