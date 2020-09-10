b54ce0a16c19f3d2b81e4f07dc6f10e6bde5cf4d
Noah <npizza10@gmail.com>
2020-09-10 00:22:12 -0600

    Finished Manhattan Distance function

--- Excercises/Excercise4/knn.py
+++ Excercises/Excercise4/knn.py
@@ -1,8 +1,13 @@
 import numpy as np
 import time
+import math
 
+'''
+Goal of this code was to modify starter code by adding:
+    1 - Manhattan distance function
+'''
 class knn(object):
-    def __init__(self,k=3,weighted=True,classify=True, distance = 'Euclidean'):  
+    def __init__(self,k=3,weighted=True,classify=True, distance =  'Manhattan'):  
         self.k = k
         self.weighted = weighted
         self.classify = classify
@@ -17,15 +22,33 @@ class knn(object):
         dist = np.sqrt(dist) 
         return  dist
     
+    '''
+    manhattan_distance() - Goal of this function is to calculate the Manhattan distance between
+                            each train sample to each test sample and log those results into a 
+                            2D matrix called 'dist'. dist[i,j] is the Manhattan distance from 
+                            training example i to test example j 
+    
+                            Returns a 2D array
+    
+    '''
     def manhattan_distance(self,X_test):
-        # Returns 2D array dist
-        # where dist[i,j] is the Manhattan distance from training example i to test example j
-        # Your code goes here
-        dist = 0
+
+        #Create dist[i,j] where each row reps a unique sample from train, and each column each reps a test sampe. 
+        dist = np.zeros((self.X_train.shape[0], X_test.shape[0]))
         
-        
-        
-        
+        #print(dist.shape)# (9000 , 1000) if n=10,000
+        for i in range(len(self.X_train)):
+            for j in range(len(X_test)):
+                               
+                '''
+                #Both ways to calculate euclideann distance | euc1 = euc2 |  - 'euc2' is used to derive manhattan equation
+                euc1 = np.linalg.norm(self.X_train[i] - X_test[j])
+                euc2 = math.sqrt(np.sum(pow( abs(self.X_train[i] - X_test[j]) , 2 )   ))
+                '''  
+                
+                #Calculate manhattan distance
+                dist[i][j] =  np.sum(abs(self.X_train[i] - X_test[j]))
+    
         return  dist
     
     def fit(self,x,y): 
@@ -36,7 +59,7 @@ class knn(object):
     def predict(self,x_test):
         if self.distance=='Euclidean':
             dist  = self.euclidean_distance(x_test)     
-        else:
+        if self.distance == 'Manhattan':
             dist  = self.manhattan_distance(x_test)     
         
         nn = np.argsort(dist,axis=0)[:self.k]
@@ -75,9 +98,16 @@ if __name__ == "__main__":
     ind = np.random.permutation(len(y))
     X=X[ind[:n]]
     y=y[ind[:n]]
-    
     X_train, X_test, y_train, y_test = split_train_test(X,y)
     
+    
+    #print(">>> Xtrain length - ", len(X_train)) #Is 9000 if n = 10,000
+    #print(">>> ytrain length - ", len(y_train)) #Is 9000 if n = 10,000
+    #print(">>> X_test length - ", len(X_test)) #Is 1,000 if n = 10,000
+
+    
+    
+    #---------- MODEL 1  = EUCLIDEAN? ---------------
     model = knn()
     start = time.time()
     model.fit(X_train, y_train)
@@ -91,6 +121,7 @@ if __name__ == "__main__":
     print('Elapsed_time testing  {0:.6f} '.format(elapsed_time))   
     print('Accuracy:',np.sum(pred==y_test)/len(y_test))
     
+    #---------- MODEL 2 = MANHATTAN --------------
     model = knn(weighted=False)
     start = time.time()
     model.fit(X_train, y_train)
