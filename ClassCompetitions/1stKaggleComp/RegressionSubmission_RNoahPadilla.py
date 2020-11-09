4b324cbe391cd4c9df51cd94ffdc6247f95a38ab
Noah <npizza10@gmail.com>
2020-11-09 03:07:32 -0700

    Finished model train and fit

--- ClassCompetitions/1stKaggleComp/RegressionSubmission_RNoahPadilla.py
+++ ClassCompetitions/1stKaggleComp/RegressionSubmission_RNoahPadilla.py
@@ -23,33 +23,74 @@ https://www.kaggle.com/c/cs-43615361-in-class-competition
     
 """
 
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
 import numpy as np
-from utils import *
 import time
-import math
-
-#Responsible for importing and extracting data into useful layout for data processing - used for x_test and x_train data
-def import_xData():
-    print("TODO")
-
-#Responsible for importing extracting data into useful layout for data processing - used for y_train
-def import_yData():
-    print("TODO")
-
-#Responsible for exporting results into a csv file - used for creating y_test
-def export_yData():
-    print("TODO")
-
+import pandas as pd
 
 if __name__ == "__main__":
-    data_path = 'C:\\Users\\npizz\\Desktop\\Machine-Learning\\ClassCompetitions\\1stKaggleComp'
+    
+    #Over 250,000 train data samples and 60,000 test data samples
+    X_train = pd.read_csv('.\\x_train.csv',header=None).to_numpy()[:100]
+    y_train = pd.read_csv('.\\y_train.csv',header=None).to_numpy()[:100]
+    X_test = pd.read_csv('.\\x_test.csv',header=None).to_numpy()[:100]
+    
+    '''
+    class sklearn.ensemble.RandomForestRegressor(n_estimators=100, *, 
+                                                 criterion='mse', max_depth=None,
+                                                 min_samples_split=2, min_samples_leaf=1,
+                                                 min_weight_fraction_leaf=0.0, 
+                                                 max_features='auto', max_leaf_nodes=None, 
+                                                 min_impurity_decrease=0.0, 
+                                                 min_impurity_split=None, 
+                                                 bootstrap=True, oob_score=False,
+                                                 n_jobs=None, random_state=None, 
+                                                 verbose=0, warm_start=False, 
+                                                 ccp_alpha=0.0, max_samples=None)
+    n_estimators - The number of trees in the forest.
+    '''
+    model = RandomForestRegressor(max_depth = 12,n_estimators=300,random_state=0)
+    
+    start = time.time()
+    model.fit(X_train, y_train)
+    elapsed_time = time.time()-start
+    print('Elapsed time training {:.6f} secs'.format(elapsed_time))
+    
+    start = time.time()
+    pred = model.predict(X_test)
+    elapsed_time = time.time()-start
+    print('Elapsed time testing {:.6f} secs'.format(elapsed_time))
+    
+    '''
+    > Submission Format <
+    Submissions to the Kaggle leaderboard must take a specific form. 
+    Submissions should be a csv with two columns, "ID" and "Prediction" and 
+    55822 rows corresponding to the test observations. Including the header row, 
+    there will be 55823 rows in the submission file. The "ID" is a unique numeric 
+    identifier from "1", "2", ... to "55822". The "Prediction" is your predicted flux 
+    for the test set.
+    
+    
+    VVVV  https://datatofish.com/export-dataframe-to-csv/ VVVV
+    
+    cars = {'Brand': ['Honda Civic','Toyota Corolla','Ford Focus','Audi A4'],
+        'Price': [22000,25000,27000,35000]
+        }
 
-    #X = np.load(data_path+'mnist_X.npy').reshape(-1,28*28)/255
-    #y = np.load(data_path+'mnist_y.npy')
+    df = pd.DataFrame(cars, columns= ['Brand', 'Price'])
+
+    '''
+    #Export the predictions
+    print("NOW EXPORTING...")
     
-    #X_train, X_test, y_train, y_test = split_train_test(X,y,seed=20)
+    #Set up the layout
+    int_indices = np.arange(1,len(pred)+1)
+    str_indices = []
+    for i in range(len(int_indices)):
+        str_indices.append(str(int_indices[i]))
     
+    df = pd.DataFrame({"ID": str_indices,
+                   "Prediction": pred })
+    df.to_csv('Predictions.csv',index=False)
+    
+    print("...FINISHED EXPORTING")
